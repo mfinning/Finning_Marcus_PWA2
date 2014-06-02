b362b080910f49503499e89993fc6f812e4aa1f6
mfinning <mfinning@gmail.com>
2014-06-02 14:18:49 -0600

    Add file and folder to GIT

---
+++ WK 1/Finning_Marcus_wk1_activity_student/js/wk1activity.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+(function($){  //10. Explain why this is used instead of a document ready function. Comment your answer out on line 4.
+
+
+//4. Add the jQuery function-select the h2 of this simple web page.
+
+
+//5. Selectors using IDS
+
+
+//6. Selectors using Classes
+
+
+//7. Manipulator
+
+
+//8. Manipulator-Before
+
+
+//9. CSS Method - Complete this line
+ $(".trip").on("mouseenter", function() {
+    $(this).__("______", "_____");
+  });
+  
+
+
+})(jQuery) //Closes main tag
