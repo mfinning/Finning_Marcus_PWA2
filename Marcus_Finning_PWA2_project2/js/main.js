e450ba7cfd27fe45240abe0f429a0a53146a7dc1
mfinning <mfinning@gmail.com>
2014-08-04 02:52:52 -0600

    Added class file

---
+++ Marcus_Finning_PWA2_project2/js/main.js
@@ -0,0 +1,59 @@
+/*  
+	Your Project Title
+	Author: You
+*/
+
+(function($){
+	
+	
+	/*
+	===============================================
+	========================= APPLICATION FUNCTIONS	
+	*/
+	
+	
+	var checkLoginState = function(){
+		$.ajax({
+			url: 'xhr/check_login.php',
+			type: 'get',
+			dataType: 'json',
+			success: function(response){
+				// if user, loadApp()
+				// if error, loadLanding()
+			}
+		});
+	};
+	
+	
+
+	// 	============================================
+	//	SETUP FOR INIT
+		
+	var init = function(){
+	
+		checkLoginState();
+	};
+	
+	
+	init();
+	
+		
+	/*
+	===============================================
+	======================================== EVENTS	
+	*/
+	
+	
+	/*	
+	==================================== END EVENTS 
+	===============================================
+	*/
+		
+		
+
+	
+})(jQuery); // end private scope
+
+
+
+
