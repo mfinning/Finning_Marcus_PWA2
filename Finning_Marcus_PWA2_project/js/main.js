5c2e0893b32eb87aea5c87f5888ec8cffd1c6b0a
mfinning <mfinning@gmail.com>
2014-08-15 17:27:02 -0600

    added icons work on according andded gallerybox

--- Finning_Marcus_PWA2_project/js/main.js
+++ Finning_Marcus_PWA2_project/js/main.js
@@ -67,6 +67,24 @@
 		$('.tooltip')
 		.css({ top: mousey, left: mousex})
 	});
+	
+	/*======================== tab according function ========================*/
+	
+	
+	$('#tabs p').hide().eq(0).show();
+	$('#tabs p:not(:first)').hide();
+	
+	$('#tabs-nav li').click(function(e){
+		e.preventDefault();
+		$('#tabs p').hide();
+	$('#tabs-nav .current').removeClass("current");
+		$(this).addClass('current');
+		var clicked = $(this).find('a:first').attr('href');
+		
+		$('#tabs ' + clicked).fadeIn("fast");
+	}).eq(0).addClass('current');
+
+		
 		
 		/*================== registration form ===============*/
 	
